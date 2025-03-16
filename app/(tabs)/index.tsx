6244fc135111402995c048736879c6fa12ce3d19
Tri <vutriminh@gmail.com>
2025-03-16 15:59:34 -0400

    add navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello Expo.</Text>
+      <Text style={styles.text}>About screen</Text>
     </View>
   );
 }
@@ -13,9 +13,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#25292e"
+    backgroundColor: "#25292e",
   },
   text: {
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
     color: "#fff",
   },
 })
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,24 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <>
+      <StatusBar style="light" />
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="+not-found"
+          options={{
+
+          }}
+        />
+      </Stack>
+    </>
   )
 }
